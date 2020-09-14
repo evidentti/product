d398a3d9ab4e3cbc15e57983b771baa789597336
Hannu Honka <hannu.honka@me.com>
2020-09-14 08:30:58 +0300

    symlink + updates

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
-#git clone --branch master https://github.com/evidentti/core.git
-git clone --branch staging git@github.com:evidentti/core.git
-#git clone --branch v1.0.0 git@github.com:evidentti/core.git --single-branch # using tag
+# git clone --branch master https://github.com/evidentti/core.git
+git clone --branch master git@github.com:evidentti/core.git
+# git clone --branch staging git@github.com:evidentti/core.git
+# git clone --branch v1.0.0 git@github.com:evidentti/core.git --single-branch # using tag
 cd core
 npm install
 npm audit
